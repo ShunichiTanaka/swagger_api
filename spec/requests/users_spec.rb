d7b6524cf7461ddd3a6f613eea2d1cefbfab6370
ShunichiTanaka <shunichi.com@gmail.com>
2018-11-12 00:49:30 +0900

    request spec 作成

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "Users", type: :request do
+  include Committee::Test::Methods
+  include Rack::Test::Methods
+
+  def committee_schema
+    @committee_schema ||=
+      begin
+        driver = Committee::Drivers::OpenAPI2.new
+        schema = JSON.parse(File.read(schema_path))
+        driver.parse(schema)
+      end
+  end
+
+  def schema_path
+    Rails.root.join('swagger', 'swagger.json')
+  end
+
+  describe "GET /" do
+    it "works! (now write some real specs)" do
+      get '/'
+      assert_schema_confirm
+    end
+  end
+end
